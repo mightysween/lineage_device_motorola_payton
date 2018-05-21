0bd0047aa1e37ffdc0991557ea55cdc05e774588
mightysween <jason@mancine.net>
2018-05-20 20:09:23 -0400

    Formatting fix
    Fix formatting and add whitespace to fix commenting

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-#    device/motorola/payton/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+#   device/motorola/payton/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+
     device/motorola/payton/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     device/motorola/payton/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     device/motorola/payton/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
